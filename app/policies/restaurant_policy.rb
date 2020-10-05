1bc0b0a6af80abd01fda529a3dd10282e87c8012
iva mudronja <iva.pilizota@gmail.com>
2020-10-05 18:48:59 +0200

    added pundit, added models: restaurant & comment

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,15 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    return true
+  end
+
+  def show?
+    return true
+  end
+end
